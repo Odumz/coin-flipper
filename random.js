6c5183196b4829c7cc29a8309dedb8369b213056
Odumz <odumz20@gmail.com>
2022-02-17 12:19:30 +0100

    random.js file created

---
+++ random.js
@@ -0,0 +1,22 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener('click', () => {
+    // console.log(document.body)
+
+    let hexColor = "#";
+
+    for (let i = 0; i < 6; i++) {
+        const randomNumber = getRandomNumber();
+        hexColor += hex[randomNumber];
+    }
+    const randomNumber = getRandomNumber();
+    document.body.style.backgroundColor = hexColor;
+    color.textContent = hexColor;
+});
+
+const getRandomNumber = () => {
+    return Math.floor(Math.random() * hex.length);
+}
